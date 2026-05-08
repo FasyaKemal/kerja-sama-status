e8f74268060243d9aa39811dc061fd2589677f0f
Kemal Fasya <kemalfasya@MacBook-Air-Kemal.local>
2026-05-08 16:22:37 +0700

    cleanup

--- test_browser.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  page.on('console', msg => console.log('CONSOLE:', msg.text()));
-  page.on('pageerror', err => console.log('ERROR:', err.message));
-  await page.goto('http://127.0.0.1:3001');
-  await browser.close();
-})();
